e24cfbc494f47363dd7c8d5509eb8a5c4629c1b8
The-Briel-Deal <gabe@fordltc.net>
2022-05-13 03:11:54 -0400

    Added character dropping

--- python_connect_four/board.py
+++ python_connect_four/board.py
@@ -1,5 +1,6 @@
 import os
 from typing import List
+from time import sleep
 
 class Board:
     def __init__(self) -> None:
@@ -8,6 +9,11 @@ class Board:
         os.system("clear")
         for row in self.matrix:
             print(row)
-    def drop_char(self,row: int, num: int) -> None:
-        self.matrix[0][row] = num
-        self.refresh_board()
+    def drop_char(self,col: int, num: int) -> None:
+        self.matrix[0][col] = num
+        row = 0
+        while True:
+            if len(self.matrix) >= row-1 and self.matrix[row-1][col] != 0:
+                break
+            self.matrix[row-1][col], self.matrix[row][col] = self.matrix[row][col], self.matrix[row-1][col]
+            self.refresh_board()
